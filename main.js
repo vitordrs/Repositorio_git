0891dde0572de040bc2e3a477fafc326806d281e
vitordrs <soaresvitor196@gmail.com>
2025-07-31 14:33:54 -0300

    implementação do exercicio ajax

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const elementoNome = document.querySelector('#nome');
+    const elementoUsuario = document.querySelector('#usuario');
+    const elementoAvatar = document.querySelector('#avatar');
+    const elementoRepositorios = document.querySelector('#repositorios');
+    const elementoSeguidores = document.querySelector('#seguidores');
+    const elementoSeguindo = document.querySelector('#seguindo');
+    const elementoLink = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/vitordrs')
+        .then(function(resposta){
+            return resposta.json();
+        })
+        .then(function(dados){
+            elementoNome.textContent = dados.name;
+            elementoUsuario.textContent = dados.login;
+            elementoAvatar.src = dados.avatar_url;
+            elementoRepositorios.textContent = dados.public_repos;
+            elementoSeguidores.textContent = dados.followers;
+            elementoSeguindo.textContent = dados.following;
+            elementoLink.href = dados.html_url;
+        });
+});
