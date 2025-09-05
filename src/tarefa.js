1ffa0763049b779abd059b3c34cd36b4539c76a4
vitordrs <soaresvitor196@gmail.com>
2025-09-05 14:04:12 -0300

    tarefa typescript

---
+++ src/tarefa.js
@@ -0,0 +1,7 @@
+function multiplicar(n1, n2) {
+    var resultado = n1 * n2;
+    return resultado;
+}
+console.log(multiplicar(10, 20));
+var saudar = function (nome) { return "Olá, " + nome; };
+console.log(saudar("Vitor"));
---
+++ src/tarefa.ts
@@ -0,0 +1,10 @@
+function multiplicar (n1: number, n2: number):number{
+    const resultado = n1 * n2;
+    return resultado;
+}
+
+console.log(multiplicar(10, 20));
+
+const saudar = (nome: string): string => "Olá, " + nome;
+
+console.log(saudar("Vitor"));
